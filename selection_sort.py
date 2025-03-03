3d3362c99a01da0c83446b7cb12f9266ef94ff3a
Viswa B <theflipper070@gmail.com>
2025-03-03 18:40:18 +0530

    selection_sort
    uploaded simple code for selection sort

---
+++ selection_sort.py
@@ -0,0 +1,9 @@
+def selection_sort(arr):
+    for i in range (0, len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i]>arr[j]:
+                arr[i],arr[j]=arr[j],arr[i]
+    
+    return arr
+
+print(selection_sort(arr))
