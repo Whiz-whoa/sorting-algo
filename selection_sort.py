dc77d39643f0d278e0f8b99c4ef6ac50adabb7ac
Viswa B <theflipper070@gmail.com>
2025-03-03 18:54:11 +0530

    update selection sort
    minor error updated

--- selection_sort.py
+++ selection_sort.py
@@ -1,9 +1,9 @@
 def selection_sort(arr):
     for i in range (0, len(arr)):
+        min_index = i
         for j in range(i+1, len(arr)):
-            if arr[i]>arr[j]:
-                arr[i],arr[j]=arr[j],arr[i]
-    
+            if arr[min_index]>arr[j]:
+                min_index = j
+        arr[i],arr[min_index] = arr[min_index],arr[i]
+        
     return arr
-
-print(selection_sort(arr))
